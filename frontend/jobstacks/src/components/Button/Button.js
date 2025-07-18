1d15cfc7832bfbf0b0f7b2856ab2d4fc29c029a8
Alan <alan.arias11@gmail.com>
2025-07-17 21:58:13 -0400

    Updated button

--- frontend/jobstacks/src/components/Button/Button.js
+++ frontend/jobstacks/src/components/Button/Button.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function Button({ children, onClick, className = '', type = 'button' }) {
+export default function Button({ children, onClick = () => {}, className = '', type = 'button' }) {
   return (
     <button
       type={type}
