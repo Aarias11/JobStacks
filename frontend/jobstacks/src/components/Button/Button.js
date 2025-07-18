b1c31c607181d3a2ad36d0ae5e06d9bba17fbb8c
Alan <alan.arias11@gmail.com>
2025-07-17 21:59:10 -0400

    Updated button prop

--- frontend/jobstacks/src/components/Button/Button.js
+++ frontend/jobstacks/src/components/Button/Button.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
-export default function Button({ children, onClick = () => {}, className = '', type = 'button' }) {
+export default function Button({ children, onClick = () => {}, className = '', type = 'button', disabled = false }) {
   return (
     <button
       type={type}
       onClick={onClick}
       className={`w-[466px] h-[43px] bg-primary flex justify-center items-center rounded-[12px] font-semibold ${className}`}
+      disabled={disabled}
     >
       {children}
     </button>
