72eb22d5c2480049ca2fabca91607a240b2a3f85
Alan <alan.arias11@gmail.com>
2025-07-18 12:32:15 -0400

    Updated CORS in server.js

--- backend/server.js
+++ backend/server.js
@@ -18,9 +18,16 @@ const allowedOrigins = [
 ];
 
 app.use(cors({
-  origin: allowedOrigins,
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    }
+    return callback(new Error('Not allowed by CORS'));
+  },
   credentials: true
 }));
+app.options('*', cors());
 app.use(cookieParser());
 
 // DB Connection
